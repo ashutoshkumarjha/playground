cab7057bb28c96641920fcb6e7d7702b4c66b0e6
ashutoshkumarjha <jha.ashutosh@gmail.com>
2024-08-10 13:39:23 +0530

    Added New Activation Functions and updated the packages
    Added activation function in nn.ts Activation classes : LEAKY_RELU, ELU, SWISH, SOFTPLUS, SINE
    Made corresponding changes in src/state.ts and index.html

--- src/state.ts
+++ src/state.ts
@@ -24,7 +24,12 @@ export let activations: {[key: string]: nn.ActivationFunction} = {
   "relu": nn.Activations.RELU,
   "tanh": nn.Activations.TANH,
   "sigmoid": nn.Activations.SIGMOID,
-  "linear": nn.Activations.LINEAR
+  "linear": nn.Activations.LINEAR,
+  "sine": nn.Activations.SINE,
+  "leaky_relu" : nn.Activations.LEAKY_RELU,
+  "elu":nn.Activations.ELU,
+  "swish":nn.Activations.SWISH,
+  "softplus":nn.Activations.SOFTPLUS
 };
 
 /** A map between names and regularization functions. */
